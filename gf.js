66c71768d9783d86ea2de2f6af65725d7882346b
greedy854 <114736063+greedy854@users.noreply.github.com>
2023-01-24 20:24:03 +0100

    Delete gf.js

--- gf.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".toggle-button").click(function() {
-      $(this).parent().find("ul").slideToggle(function() {
-        // Animation complete.
-      });
-    });
-  })
